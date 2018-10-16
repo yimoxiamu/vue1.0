16ba61c6af0900f69ffd5999cffbcadfabf3e2bb
yimoxiamu <2959623954@qq.com>
2018-10-16 22:35:05 +0800

    blog info update

--- src/components/blog_info.vue
+++ src/components/blog_info.vue
@@ -5,20 +5,28 @@
 
 
 <template>
-  <div v-if="blog_info != null">    
+  <div v-if="blog_info != null"> 
+    <Row>
+      <Col class="headBox" span="2"></Col>
+      <Col class="headBox" span="20">
+          <Card class="head-card">
+          </Card>
+      </Col>
+      <Col class="headBox" span="2"></Col>
+    </Row>    
     <Row>
       <Col class="title" span="6"></Col>
-      <Col class="title" span="12"><span>{{blog_info.blog_title}}</span></Col>
+      <Col class="title" span="12"><p style="font-size: 30px;">{{blog_info.blog_title}}</p></Col>
       <Col class="title" span="6"></Col>
     </Row> 
      <Row>
       <Col class="author" span="6"></Col>
-      <Col class="author" span="12"><span >{{blog_info.create_by}}</span></Col>
+      <Col class="author" span="12" style="font-size: 20px;"><p >{{blog_info.create_by}}</p></Col>
       <Col class="author" span="6"></Col>
     </Row> 
     <Row>
       <Col class="description" span="3"></Col>
-      <Col class="description" span="18"><span>{{blog_info.blog_description}}</span></Col>
+      <Col class="description desmain " span="18"><p>{{blog_info.blog_description}}</p></Col>
       <Col class="description" span="3"></Col>
     </Row> 
     <Row>
@@ -26,10 +34,21 @@
       <Col class="text" span="18"><div v-html="blog_info.blog_text"></div></Col>
       <Col class="text" span="3"></Col>
     </Row> 
-    <Row>
-      <Col class="titlebox" span="8" style="background-color:green" >asdas</Col>
-      <Col class="titlebox" span="8" ></Col>
-      <Col class="titlebox" span="8" style="background-color:green">asdas</Col>
+    <Row v-if="title_box != null">
+      <Col class="titlebox" span="3" ></Col>
+      <Col class="titlebox" span="18" > 
+        <a><p @click="doRout($event)" :data-id = "title_box[0].id" style="float:left"><strong>上一篇：</strong>{{title_box[0].blog_title}}</p> </a>
+        <a><p @click="doRout($event)" style="float:right"  :data-id = "title_box[1].id"><strong>下一篇：</strong>{{title_box[1].blog_title}}</p></a>
+      </Col>
+      <Col class="titlebox" span="3" ></Col>
+    </Row> 
+    <Row v-if="title_box == null">
+      <Col class="titlebox" span="3" ></Col>
+      <Col class="titlebox" span="18">
+        <a><p @click="doRout($event)" :data-id = "last_id" style="float:left"> <strong>上一篇：</strong>{{last_title}}</p></a> 
+        <a><p @click="doRout($event)" style="float:right" :data-id = "next_id"><strong>下一篇：</strong>{{next_title}}</p></a>
+      </Col>
+      <Col class="titlebox" span="3" ></Col>
     </Row> 
 
   </div>
@@ -43,7 +62,11 @@ export default {
     return {
         id:null,
         blog_info:null,
-        title_box:[]
+        title_box:null,
+        next_title:null,
+        next_id:null,
+        last_title:null,
+        last_id:null
     }
   },
   created(){
@@ -53,7 +76,22 @@ export default {
       if(response.data.code == 0){
       let data = response.data.data;
       this.blog_info = data.blogInfo;
-      this.title_box = data.contextualInfo;
+      if(data.contextualInfo.length == 2){
+          this.title_box = data.contextualInfo;
+      }else{
+          if(data.contextualInfo[0].id < this.id){
+            this.last_title = data.contextualInfo[0].blog_title;
+            this.next_title = "没有啦！";
+            this.last_id = data.contextualInfo[0].id;
+            this.next_id = "null";
+          }else{
+            this.next_title = data.contextualInfo[0].blog_title;
+            this.last_title = "没有啦！";
+            this.next_id = data.contextualInfo[0].id;
+            this.last_id = "null";
+          }
+      }
+      
       }else{
         this.$Message.error(response.data.message);
       }
@@ -62,6 +100,46 @@ export default {
     })
   },
   mounted(){
+  },
+  methods:{
+    doRout(e){
+      var id = e.target.dataset.id;
+      if( id == "null"){
+        return;
+      }
+      this.$router.push({name:'info',params :{blogId:id}});
+
+
+      this.id = this.$route.params.blogId;
+      let url = this.$comjs.buildPath0('blog/blogInfo',this.$route.params.blogId);
+      this.$http.get(url).then(response =>{
+      if(response.data.code == 0){
+      let data = response.data.data;
+      this.blog_info = data.blogInfo;
+      if(data.contextualInfo.length == 2){
+          this.title_box = data.contextualInfo;
+      }else{
+          this.title_box = null;
+          if(data.contextualInfo[0].id < this.id){
+            this.last_title = data.contextualInfo[0].blog_title;
+            this.next_title = "没有啦！";
+            this.last_id = data.contextualInfo[0].id;
+            this.next_id = "null";
+          }else{
+            this.next_title = data.contextualInfo[0].blog_title;
+            this.last_title = "没有啦！";
+            this.next_id = data.contextualInfo[0].id;
+            this.last_id = "null";
+          }
+      }
+      
+      }else{
+        this.$Message.error(response.data.message);
+      }
+    }).catch(error => {
+      console.info(error)
+    })
+    }
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,6 @@ export default new Router({
   routes: [
     {
       path : '/',
-      name : 'index',
       component : Index,
       children:[
         {
